f7279645e754c230cc408e708f1a87b1c136cc28
rakeshevon <33094655+rakeshevon@users.noreply.github.com>
2018-02-06 20:00:31 +0530

    Add Updating files

--- ang-learn-first/src/app/dynamic-data-table/dynamic-data-table.component.ts
+++ ang-learn-first/src/app/dynamic-data-table/dynamic-data-table.component.ts
@@ -10,33 +10,13 @@ export class DynamicDataTableComponent implements OnInit {
   datas: any;
   constructor() {
     this.formData = {};
-    // this.datas = [{
-    //   // fName: '',
-    //   // lName: '',
-    //   // age: '',
-    //   // expr: '',
-    // }];
     this.datas = [];
   }
   addData() {
-    console.log(this.formData);
-    console.log(this.datas);
-    //this.datas.push(this.formData);
-    //this.formData = {};
-   // const fitlerData = this.datas.trim();
-    //console.log(fitlerData);
-    if ((this.datas = []) === null) {
-        console.log('No Data');
-      }else {
-        console.log('Yes Data');
-      }
+    this.datas.push(this.formData);
+    this.formData = {};
   }
 
   ngOnInit() {
-    // if (this.formData == null) {
-    //   console.log('No Data');
-    // }else {
-    //   console.log('Yes Data');
-    // }
   }
 }
