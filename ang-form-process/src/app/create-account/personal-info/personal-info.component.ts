86e12e2935e827d93d5e9254bb25670485a68651
Rishi <rakesh.evontech@gmail.com>
2018-02-20 19:24:28 +0530

    Add files via upload

--- ang-form-process/src/app/create-account/personal-info/personal-info.component.ts
+++ ang-form-process/src/app/create-account/personal-info/personal-info.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.css', './../create-account.component.css']
 })
 export class PersonalInfoComponent implements OnInit {
+ item: number
+  constructor() {
+  }
 
-  constructor() { }
+
+  tabContent(item) {
+    console.log(this);
+  }
+  containerIndx(item) {
+    console.log(this);
+  }
 
   ngOnInit() {
+    console.log("testing 01");
+    this.item = 1;
+    console.log(this.item);
+
+
   }
 
 }
